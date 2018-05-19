94a7a14a3831a030b3862713a2d087bcf762da42
sPhinx <ankit.rajput@instavans.com>
2018-05-20 03:50:39 +0530

    update: ticket adapter created for holding data.

--- app/src/main/java/com/ar/sphinx/rxflight/MainActivity.java
+++ app/src/main/java/com/ar/sphinx/rxflight/MainActivity.java
@@ -3,11 +3,19 @@ package com.ar.sphinx.rxflight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ar.sphinx.rxflight.network.model.Ticket;
+import com.ar.sphinx.rxflight.view.TicketAdapter;
+
+public class MainActivity extends AppCompatActivity implements TicketAdapter.TicketAdapterListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void onTickSelection(Ticket ticket) {
+		//do some thing when it clicked.
 	}
 }
---
+++ app/src/main/java/com/ar/sphinx/rxflight/view/TicketAdapter.java
@@ -0,0 +1,128 @@
+package com.ar.sphinx.rxflight.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ar.sphinx.rxflight.R;
+import com.ar.sphinx.rxflight.network.model.Ticket;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.github.ybq.android.spinkit.SpinKitView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class TicketAdapter extends RecyclerView.Adapter<TicketAdapter.MyViewHolder> {
+
+	private Context context;
+	private TicketAdapterListener listener;
+	private List<Ticket> ticketList;
+
+	public TicketAdapter(Context context,List<Ticket> list,TicketAdapterListener listener) {
+		this.context = context;
+		this.ticketList = list;
+		this.listener = listener;
+	}
+
+	class MyViewHolder extends RecyclerView.ViewHolder{
+
+		@BindView(R.id.tv_airline_name)
+		private TextView airlineName;
+
+		@BindView(R.id.logo)
+		private ImageView logo;
+
+		@BindView(R.id.tv_num_stops)
+		private TextView stops;
+
+		@BindView(R.id.tv_seats)
+		private TextView seats;
+
+		@BindView(R.id.tv_departure)
+		private TextView deaprture;
+
+		@BindView(R.id.tv_arrival)
+		private TextView arrival;
+
+		@BindView(R.id.tv_time)
+		private TextView duration;
+
+		@BindView(R.id.price)
+		private TextView price;
+
+		@BindView(R.id.loader)
+		private SpinKitView loader;
+
+		public MyViewHolder(View itemView) {
+			super(itemView);
+			ButterKnife.bind(this,itemView);
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					listener.onTickSelection(ticketList.get(getAdapterPosition()));
+				}
+			});
+
+		}
+	}
+
+	@NonNull
+	@Override
+	public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		View view = LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.layout_ticket,parent,false);
+		return new MyViewHolder(view);
+	}
+
+	@SuppressLint("DefaultLocale")
+	@Override
+	public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+		final Ticket ticket = ticketList.get(position);
+
+		Glide.with(context)
+				.load(ticket.getAirline().getLogo())
+				.apply(RequestOptions.circleCropTransform())
+				.into(holder.logo);
+
+		holder.airlineName.setText(ticket.getAirline().getName());
+
+		holder.deaprture.setText(String.format("%s Dep", ticket.getDeparture()));
+		holder.arrival.setText(String.format("%s Dest", ticket.getArrival()));
+
+		holder.duration.setText(ticket.getFlightNumber());
+		holder.duration.append(", " + ticket.getDuration());
+		holder.stops.setText(String.format("%d Stops", ticket.getNumberOfStops()));
+
+		if (!TextUtils.isEmpty(ticket.getInstructions())) {
+			holder.duration.append(", " + ticket.getInstructions());
+		}
+
+		if (ticket.getPrice() != null) {
+			holder.price.setText(String.format("₹%s", String.format("%.0f", ticket.getPrice().getPrice())));
+			holder.seats.setText(String.format("%s Seats", ticket.getPrice().getSeats()));
+			holder.loader.setVisibility(View.INVISIBLE);
+		} else {
+			holder.loader.setVisibility(View.VISIBLE);
+		}
+	}
+
+	@Override
+	public int getItemCount() {
+		return ticketList.size();
+	}
+
+	public interface TicketAdapterListener {
+		void onTickSelection(Ticket ticket);
+	}
+}
